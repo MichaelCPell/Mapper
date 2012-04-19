70f72e42891aac47d6ba5384eacf69b7302c125d
Michael <michael@ubuntu.(none)>
2012-04-19 14:22:25 -0400

    with pagination

--- app/controllers/coordinates_controller.rb
+++ app/controllers/coordinates_controller.rb
@@ -1,4 +1,6 @@
 class CoordinatesController < ApplicationController
+  #require 'will_paginate/array'
+
   # GET /coordinates
   # GET /coordinates.json
   def index
@@ -12,7 +14,7 @@ class CoordinatesController < ApplicationController
       @coordinates = @coordinates.where(:term => params[:term])
     end
 
-
+    @coordinates = @coordinates.paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erb
